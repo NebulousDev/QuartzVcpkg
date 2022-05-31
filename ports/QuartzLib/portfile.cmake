f5e939e221c166a99a8c6a98ebc73686a6f8b19f
NebulousDev <nebulousdev@gmail.com>
2022-05-31 16:21:38 -0400

    Added quartzlib hash

--- ports/QuartzLib/portfile.cmake
+++ ports/QuartzLib/portfile.cmake
@@ -1,6 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO NebulousDev/QuartzLib
+  REF 176bf37ba37ed77ba44addc25e636ae6d57dffb3
+  SHA512 b52cc2760507061b8f72da31d24a2fe1b505bb253161383e12075acabb192f46ebacc24bd0f15a00d64537e5a41c39bd9417de3293da1c9c55df35d97ce5619f
   HEAD_REF main
 )
 
