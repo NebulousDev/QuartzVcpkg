897df6f31b9738c0938f4bf42bcf5ff6fb67c7e1
NebulousDev <nebulousdev@gmail.com>
2022-05-31 14:00:14 -0400

    Added QuartzLib ports

---
+++ ports/QuartzLib/portfile.cmake
@@ -0,0 +1,20 @@
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO NebulousDev/QuartzLib
+  HEAD_REF main
+)
+
+vcpkg_configure_cmake(
+  SOURCE_PATH "${SOURCE_PATH}"
+  PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
