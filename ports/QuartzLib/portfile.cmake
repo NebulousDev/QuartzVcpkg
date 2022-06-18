63817ef6bc8366e6550b6d348060f4361e9d32fb
NebulousDev <nebulousdev@gmail.com>
2022-06-18 12:04:14 -0400

    QuartzLib v1.0.5 - fix

--- ports/QuartzLib/portfile.cmake
+++ ports/QuartzLib/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO NebulousDev/QuartzLib
   REF 0b475788e1cf75bc8ed6ee5b55d4c2137203fda3
-  SHA512 0
+  SHA512 8cb117e13d1afd9293b9df7aaa86e6c18c88ac4afcc0c274325c798e219a4a0a9183d0186f5fe588fc0f9f0ee7cce0c181d689f07eebbd8ba1f1b8770f5cce88
   HEAD_REF main
 )
 
